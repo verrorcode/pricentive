1375e442cfa41152cb6909b4b7f93b3790ba247c
Vaibhav5 Gupta <vaibhavgupta.xl@gmail.com>
2025-11-14 20:09:32 +0530

    Fix basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/pricentive' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pricentive' : '',
+  basePath: '/pricentive',
+  assetPrefix: '/pricentive',
 };
 
 export default nextConfig;
